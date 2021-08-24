7ed10e7a7f282c20fec196a5b9f7eaa3b833645c
Andrew Martinez <andrewmartinez6313@gmail.com>
2021-08-23 22:33:38 -0500

    loaded time block data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-// NEED TO SET CURRENT TIME AND DISPLAY IT AT THE TOP OF THE CALENDAR
+// NEED TO SET CURRENT DAY AND DISPLAY IT AT THE TOP OF THE CALENDAR
 var timeEl = $("#currentDay");
-var currentTime;
+var currentDay;
 // call function to update clock
 updateClock();
 // NEED TO LOAD TIME BLOCK DATA WHEN PAGE IS OPENED TO SHOW STANDARD BUSINESS HOURS
-
+loadTimeBlockData();
 
 
 
@@ -13,8 +13,8 @@ var currentClockTime = setInterval(updateClock, 1000)
 
 // NEED TO CREATE A FUNCTION TO SET CURRENT TIME AND CHECK TIME BLOCKS
 function updateClock() {
-    currentTime = moment().format('MMMM Do, YYYY');
-    timeEl.text(currentTime);
+    currentDay = moment().format('MMMM Do, YYYY');
+    timeEl.text(currentDay);
     // function to check the time block
 }
 
@@ -29,3 +29,16 @@ function updateClock() {
 // SAVE INPUT TO LOCAL STORAGE BY CREATING A FUNCTION TO CLEAR INFO
 
 // NEED TO LOAD TIME BLOCK DATA FROM LOCAL STORAGE TO EACH COORDINATING TIME BLOCK
+loadTimeBlockData() {
+    $('#8 .description').val(localStorage.getItem('8'));
+    $('#9 .description').val(localStorage.getItem('9'));
+    $('#10 .description').val(localStorage.getItem('10'));
+    $('#11 .description').val(localStorage.getItem('11'));
+    $('#12 .description').val(localStorage.getItem('12'));
+    $('#1 .description').val(localStorage.getItem('1'));
+    $('#2 .description').val(localStorage.getItem('2'));
+    $('#3 .description').val(localStorage.getItem('3'));
+    $('#4 .description').val(localStorage.getItem('4'));
+    $('#5 .description').val(localStorage.getItem('5'));
+    $('#6 .description').val(localStorage.getItem('6'));
+}
